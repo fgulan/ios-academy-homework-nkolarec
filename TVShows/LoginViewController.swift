7cdeabb6d675be199649a8d98bb062dad9ac7ae6
Nina Kolarec <nina.kolarec7@gmail.com>
2019-07-17 21:43:09 +0200

    Changes before merge

--- TVShows/LoginViewController.swift
+++ TVShows/LoginViewController.swift
@@ -13,6 +13,7 @@ final class LoginViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     
     //MARK: - Properties
     private var numberOfTaps: Int = 0
@@ -21,12 +22,17 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.activityIndicator.startAnimating()
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.stopAnimating()
         self.label.text = String(numberOfTaps)
         self.button.layer.cornerRadius = 10
     }
     
     //MARK: - Actions
     @IBAction private func buttonPressed(_ sender: UIButton) {
+        activityIndicator.startAnimating()
+        activityIndicator.stopAnimating()
         numberOfTaps += 1
         label.text = String(numberOfTaps)
     }
