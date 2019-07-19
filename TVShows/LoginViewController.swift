e3545f76cf507df0c171244f44a69fa75bed44c8
Nina Kolarec <nina.kolarec7@gmail.com>
2019-07-20 00:39:21 +0200

    Added check for text fields and api calls for login and register

--- TVShows/LoginViewController.swift
+++ TVShows/LoginViewController.swift
@@ -19,6 +19,7 @@ final class LoginViewController: UIViewController {
     @IBOutlet private weak var buttonCheck: UIButton!
     @IBOutlet private weak var buttonLogIn: UIButton!
     @IBOutlet private weak var buttonCreateAccount: UIButton!
+    @IBOutlet private weak var _infoLabel: UILabel!
     
     //MARK: - Properties
     private var isChecked: Bool = false
@@ -44,8 +45,81 @@ final class LoginViewController: UIViewController {
     }
         
     @IBAction func registerUser(_ sender: UIButton) {
+        if(usernameTextField.text!.isEmpty && passwordTextField.text!.isEmpty){
+            print("One of the fields is empty")
+        } else {
+            _registerUserWith(email: usernameTextField.text!, password: passwordTextField.text!)
+        }
+        
     }
     
     @IBAction func logInUser(_ sender: Any) {
+        if(usernameTextField.text!.isEmpty && passwordTextField.text!.isEmpty){
+            print("One of the fields is empty")
+        } else {
+            _loginUserWith(email: usernameTextField.text!, password: passwordTextField.text!)
+        }
     }
+}
+
+// MARK: - Register
+private extension LoginViewController {
+    
+    func _registerUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+        
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
+        Alamofire
+            .request(
+                "https://api.infinum.academy/api/users",
+                method: .post,
+                parameters: parameters,
+                encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) { (response: DataResponse<User>) in
+                switch response.result {
+                case .success(let user):
+                    print("Success: \(user)")
+                case .failure(let error):
+                    print("API failure: \(error)")
+                }
+        }
+        SVProgressHUD.dismiss()
+    }
+    
+}
+
+// MARK: - Login
+private extension LoginViewController {
+    
+    func _loginUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        Alamofire
+            .request(
+                "https://api.infinum.academy/api/users/sessions",
+                method: .post,
+                parameters: parameters,
+                encoding: JSONEncoding.default)
+            .validate()
+            .responseJSON { [weak self] dataResponse in
+                switch dataResponse.result {
+                case .success(let response):
+                    self?._infoLabel.text = "Success: \(response)"
+                    SVProgressHUD.showSuccess(withStatus: "Success")
+                case .failure(let error):
+                    self?._infoLabel.text = "API failure: \(error)"
+                    SVProgressHUD.showError(withStatus: "Failure")
+                }
+        }
+        SVProgressHUD.dismiss()
+    }
+    
 }
